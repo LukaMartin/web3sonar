6efd38326a92be7b1fc14e3b35ee0ef63dff53e5
Luka Martin <ldm3334@gmail.com>
2024-06-29 10:16:25 +1000

    Add hyperlink to BlockNative in footer.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="h-32 max-w-7xl flex justify-center sm:justify-start items-center mt-20 mx-auto px-4 md:px-8 border-t border-white/10">
       <p className="text-white/75 italic">
-        Built using the BlockNative Gas API
+        Built using the <a href="https://www.blocknative.com/" target="_blank" className="text-white hover:text-white/75">BlockNative</a> Gas API
       </p>
     </footer>
   );
