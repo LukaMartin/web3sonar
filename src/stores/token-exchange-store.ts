a8b14d758b7fdea3d397b4b74a698fec5a499375
Luka Martin <ldm3334@gmail.com>
2024-07-10 17:44:31 +1000

    Futher update to quote fetching and sending transaction error handling.

--- src/stores/token-exchange-store.ts
+++ src/stores/token-exchange-store.ts
@@ -5,157 +5,176 @@ import { JsonRpcSigner } from "ethers";
 import { create } from "zustand";
 
 type Store = {
-    fromChain: string;
-    toChain: string;
-    fromToken: string;
-    toToken: string;
-    fromAmount: number;
-    txResult: TokenExchangeResult | null;
-    awaitingConfirmation: boolean;
-    settingAllowance: boolean;
-    quote: Quote | null;
-    isLoading: boolean;
-    insufficientFunds: boolean;
-    fetchQuoteErrorMessage: string;
-    allowanceErrorMessage: string;
-    transactionErrorMessage: string;
-    setFromChain: (chain: string) => void;
-    setToChain: (chain: string) => void;
-    setFromToken: (token: string) => void;
-    setToToken: (token: string) => void;
-    setFromAmount: (amount: number) => void;
-    setTxResult: (result: TokenExchangeResult | null) => void;
-    setAwaitingConfirmation: (value: boolean) => void;
-    setSettingAllowance: (value: boolean) => void;
-    fetchQuote: (address: `0x${string}`, fromAmount: number) => void;
-    exchangeTokens: (txSigner: JsonRpcSigner | undefined, clearInput: () => void) => void
-}
+  fromChain: string;
+  toChain: string;
+  fromToken: string;
+  toToken: string;
+  fromAmount: number;
+  txResult: TokenExchangeResult | null;
+  awaitingConfirmation: boolean;
+  settingAllowance: boolean;
+  quote: Quote | null;
+  isLoading: boolean;
+  insufficientFunds: boolean;
+  fetchQuoteErrorMessage: string;
+  allowanceErrorMessage: string;
+  transactionErrorMessage: string;
+  setFromChain: (chain: string) => void;
+  setToChain: (chain: string) => void;
+  setFromToken: (token: string) => void;
+  setToToken: (token: string) => void;
+  setFromAmount: (amount: number) => void;
+  setTxResult: (result: TokenExchangeResult | null) => void;
+  setAwaitingConfirmation: (value: boolean) => void;
+  setSettingAllowance: (value: boolean) => void;
+  fetchQuote: (address: `0x${string}`, fromAmount: number) => void;
+  exchangeTokens: (
+    txSigner: JsonRpcSigner | undefined,
+    clearInput: () => void
+  ) => void;
+};
 export const useTokenExchangeStore = create<Store>((set, get) => ({
-    fromChain: "",
-    toChain: "",
-    fromToken: "",
-    toToken: "",
-    fromAmount: 0,
-    fromAddress: "",
-    txResult: null,
-    awaitingConfirmation: false,
-    settingAllowance: false,
-    quote: null,
-    isLoading: false,
-    insufficientFunds: false,
-    fetchQuoteErrorMessage: "",
-    allowanceErrorMessage: "",
-    transactionErrorMessage: "",
-    setFromChain: (chain: string) => {
-        set(() => ({
-            fromChain: chain
-        }))
-    },
-    setToChain: (chain: string) => {
-        set(() => ({
-            toChain: chain
-        }))
-    },
-    setFromToken: (token: string) => {
-        set(() => ({
-            fromToken: token
-        }))
-    },
-    setToToken: (token: string) => {
-        set(() => ({
-            toToken: token
-        }))
-    },
-    setFromAmount: (amount: number) => {
-        set(() => ({
-            fromAmount: amount
-        }))
-    },
-    setTxResult: (result: TokenExchangeResult | null) => {
-        set(() => ({
-            txResult: result
-        }))
-    },
-    setAwaitingConfirmation: (value: boolean) => {
-        set(() => ({
-            awaitingConfirmation: value
-        }))
-    },
-    setSettingAllowance: (value: boolean) => {
-        set(() => ({
-            settingAllowance: value
-        }))
-    },
-    fetchQuote: async (address: `0x${string}`, fromAmount: number) => {
-        set(() => ({
-            isLoading: true
-        }))
-        
-        try {
-            const response = await fetch(
-                `https://li.quest/v1/quote?fromChain=${get().fromChain}&toChain=${get().toChain}&fromToken=${get().fromToken}&toToken=${get().toToken}&fromAmount=${fromAmount}&fromAddress=${address}`,
-                {
-                    method: "GET",
-                }
-            );
+  fromChain: "",
+  toChain: "",
+  fromToken: "",
+  toToken: "",
+  fromAmount: 0,
+  fromAddress: "",
+  txResult: null,
+  awaitingConfirmation: false,
+  settingAllowance: false,
+  quote: null,
+  isLoading: false,
+  insufficientFunds: false,
+  fetchQuoteErrorMessage: "",
+  allowanceErrorMessage: "",
+  transactionErrorMessage: "",
+  setFromChain: (chain: string) => {
+    set(() => ({
+      fromChain: chain,
+    }));
+  },
+  setToChain: (chain: string) => {
+    set(() => ({
+      toChain: chain,
+    }));
+  },
+  setFromToken: (token: string) => {
+    set(() => ({
+      fromToken: token,
+    }));
+  },
+  setToToken: (token: string) => {
+    set(() => ({
+      toToken: token,
+    }));
+  },
+  setFromAmount: (amount: number) => {
+    set(() => ({
+      fromAmount: amount,
+    }));
+  },
+  setTxResult: (result: TokenExchangeResult | null) => {
+    set(() => ({
+      txResult: result,
+    }));
+  },
+  setAwaitingConfirmation: (value: boolean) => {
+    set(() => ({
+      awaitingConfirmation: value,
+    }));
+  },
+  setSettingAllowance: (value: boolean) => {
+    set(() => ({
+      settingAllowance: value,
+    }));
+  },
+  fetchQuote: async (address: `0x${string}`, fromAmount: number) => {
+    set(() => ({
+      isLoading: true,
+    }));
 
-            if (!response.ok) {
-                throw new Error();
-            }
-    
-            const data = await response.json();
-    
-            if (data.message) {
-                set(() => ({
-                quote: data
-            }))
-            } else if (!data.message) {
-                set(() => ({
-                    quote: data
-                }))
-            }  
-       
-            setTimeout(() => {
-                set(() => ({
-                    isLoading: false
-                }))
-            }, 3000);
-        } catch(error) {
-            set(() => ({
-                fetchQuoteErrorMessage: "Error fetching quote. Please try again."
-            }))
-            return;
-        }     
-    },
-    exchangeTokens: async (txSigner: JsonRpcSigner | undefined, clearInput: () => void) => {
+    try {
+      const response = await fetch(
+        `https://li.quest/v1/quote?fromChain=${get().fromChain}&toChain=${
+          get().toChain
+        }&fromToken=${get().fromToken}&toToken=${
+          get().toToken
+        }&fromAmount=${fromAmount}&fromAddress=${address}`,
+        {
+          method: "GET",
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+
+      if (data.message) {
         set(() => ({
-            awaitingConfirmation: true
-        }))
+          quote: data,
+        }));
+      } else if (!data.message) {
         set(() => ({
-            txResult: null
-        }))
-        const signer = txSigner;
+          quote: data,
+        }));
+      }
+
+      setTimeout(() => {
+        set(() => ({
+          isLoading: false,
+        }));
+      }, 3000);
+    } catch (error) {
+      set(() => ({
+        fetchQuoteErrorMessage: "Error fetching quote. Please try again.",
+      }));
+      set(() => ({
+        isLoading: false,
+      }));
+      return;
+    }
+  },
+  exchangeTokens: async (
+    txSigner: JsonRpcSigner | undefined,
+    clearInput: () => void
+  ) => {
+    set(() => ({
+      awaitingConfirmation: true,
+    }));
+    set(() => ({
+      txResult: null,
+    }));
+    const signer = txSigner;
 
     if (get().fromToken !== "ETH") {
       set(() => ({
-        settingAllowance: true
-      }))
+        settingAllowance: true,
+      }));
       try {
         await checkAndSetAllowance({
-            wallet: signer!,
-            tokenAddress: get().quote!.action.fromToken.address,
-            approvalAddress: get().quote!.estimate.approvalAddress,
-            amount: get().fromAmount,
-            fromToken: get().fromToken,
-          });
+          wallet: signer!,
+          tokenAddress: get().quote!.action.fromToken.address,
+          approvalAddress: get().quote!.estimate.approvalAddress,
+          amount: get().fromAmount,
+          fromToken: get().fromToken,
+        });
         set(() => ({
-            settingAllowance: false
-        }))
-      } catch(error) {
-        console.log("ERROR", error)
-        set(() => ({
-            allowanceErrorMessage: "Set allowance failed. Please try again."
-        }))
+          settingAllowance: false,
+        }));
+      } catch (error: any) {
+        console.log("ERROR", error);
+        if (error.message.includes("user rejected action")) {
+          set(() => ({
+            transactionErrorMessage: "User rejected transaction.",
+          }));
+        } else {
+          set(() => ({
+            allowanceErrorMessage: "Set allowance failed. Please try again.",
+          }));
+        }
         return;
       }
     }
@@ -163,18 +182,24 @@ export const useTokenExchangeStore = create<Store>((set, get) => ({
     let tx;
 
     try {
-        tx = await signer!.sendTransaction(get().quote!.transactionRequest);
+      tx = await signer!.sendTransaction(get().quote!.transactionRequest);
+      set(() => ({
+        awaitingConfirmation: false,
+      }));
+      await tx.wait();
+    } catch (error: any) {
+      console.log("ERROR", error);
+      if (error.message.includes("user rejected action")) {
         set(() => ({
-            awaitingConfirmation: false
-        }))
-        await tx.wait();
-    } catch(error) {
+          transactionErrorMessage: "User rejected transaction.",
+        }));
+      } else {
         set(() => ({
-            transactionErrorMessage: "Transaction failed. Please try again."
-        }))
-        return;
+          transactionErrorMessage: "Transaction failed. Please try again.",
+        }));
+      }
+      return;
     }
-    
 
     let result: TokenExchangeResult | null;
 
@@ -187,8 +212,8 @@ export const useTokenExchangeStore = create<Store>((set, get) => ({
       });
 
       set(() => ({
-        txResult: result
-      }))
+        txResult: result,
+      }));
       if (result!.status === "DONE" || result!.status === "FAILED") {
         clearInterval(interval);
       }
@@ -196,9 +221,9 @@ export const useTokenExchangeStore = create<Store>((set, get) => ({
 
     setTimeout(() => {
       set(() => ({
-        fromAmount: 0
-      }))
+        fromAmount: 0,
+      }));
       clearInput();
     }, 5000);
-    },
+  },
 }));
