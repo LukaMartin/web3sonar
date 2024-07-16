3f6283c39096a23406894a9ec5b857a84c3a6c88
Luka Martin <ldm3334@gmail.com>
2024-07-17 09:36:25 +1000

    Add alert for info on LIFI exploit.

--- src/app/token-exchange/page.tsx
+++ src/app/token-exchange/page.tsx
@@ -8,11 +8,43 @@ const DynamicTokenExchangeInterface = dynamic(
     loading: () => <TokenExchangeInterfaceSkeleton />,
   }
 );
+import { IoAlertCircleOutline } from "react-icons/io5";
 
 export default function TokenExchange() {
   return (
     <>
       <main className="hidden xl:block mb-20">
+        <section className="w-[375px] mx-auto -mt-10 mb-5 bg-red-500 border-white/20 border-[1px] rounded-md p-2">
+          <p className="flex items-center gap-x-1 font-semibold">
+            <IoAlertCircleOutline size={25} /> Alert
+          </p>
+          <p className="text-sm">
+            {" "}
+            Earlier today LI.FI was subject to a smart contract exploit. It
+            affected users that had manually set infinite approvals. This is not
+            possible through Web 3 Sonar.
+          </p>
+          <p className="text-sm">
+            The situation has since been contained, It poses no further risk to
+            users as reported by LI.FI. Please refer to the LI.FI{" "}
+            <a
+              className="underline hover:text-white/60"
+              href="https://x.com/lifiprotocol/status/1813238237642608864"
+              target="_blank"
+            >
+              Twitter
+            </a>{" "}
+            or{" "}
+            <a
+              className="underline hover:text-white/60"
+              href="https://discord.com/invite/lifi"
+              target="_blank"
+            >
+              Discord
+            </a>{" "}
+            for further information.
+          </p>
+        </section>
         <DynamicTokenExchangeInterface />
       </main>
 
