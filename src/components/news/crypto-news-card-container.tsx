01ab01d97dfb48f0ff73b69dc69bbf8fd6332c5c
Luka Martin <ldm3334@gmail.com>
2024-07-16 11:34:41 +1000

    Add hyperlink to news article images.

--- src/components/news/crypto-news-card-container.tsx
+++ src/components/news/crypto-news-card-container.tsx
@@ -1,5 +1,5 @@
 import { CryptoNewsData } from "@/lib/types";
-import { shortenNewsSource, shortenNewsTitle } from "@/lib/utils";
+import { shortenNewsTitle } from "@/lib/utils";
 import Image from "next/image";
 
 type CryptoNewsCardContainerProps = {
@@ -18,21 +18,23 @@ export default function CryptoNewsCardContainer({
             className="flex flex-col justify-between p-6 flex-1 basis-80 max-w-[475px] sm:max-w-[48%] max-h-[450px] bg-white/[3%] border-white/20 border-[1px] rounded-md shadow-[0_7px_7px_rgba(2,2,2,1)] transition hover:scale-[1.025] overflow-hidden"
           >
             <a href={article.news_url} target="_blank">
-              <p className="mb-4 font-semibold hover:text-white/60">
+              <p className="mb-4 font-semibold hover:text-white/70">
                 {shortenNewsTitle(article.title)}
               </p>
             </a>
             <div>
-              <Image
-                src={article.image_url}
-                alt="News article image"
-                height={350}
-                width={500}
-                className="rounded-md mb-6 mt-2 object-cover"
-              />
+              <a href={article.news_url} target="_blank">
+                <Image
+                  src={article.image_url}
+                  alt="News article image"
+                  height={350}
+                  width={500}
+                  className="rounded-md mb-6 mt-2 object-cover"
+                />
+              </a>
               <div className="flex justify-between">
                 <div className="flex flex-col">
-                  <p>{shortenNewsSource(article.source_name)}</p>
+                  <p>{article.source_name}</p>
                   <p className="text-sm text-white/60">Source</p>
                 </div>
                 <div className="flex flex-col">
