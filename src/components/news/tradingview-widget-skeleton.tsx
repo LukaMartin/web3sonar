2554687cc7bff5870f85b8aec905d80a5cb8593b
Luka Martin <ldm3334@gmail.com>
2024-07-16 18:20:25 +1000

    Remove tradingview widget.

--- src/components/news/tradingview-widget-skeleton.tsx
+++
@@ -1,6 +0,0 @@
-export default function TradingviewWidgetSkeleton() {
-  return (
-    <section className="w-[100%] h-[640px] bg-white/10 border-[1px] border-white/20 rounded-md animate-pulseStrong">
-    </section>
-  )
-}
